314ae8a55e8d7546883e937e353e6397e13b8b93
Gerson Castillo <gocastillo@unah.hn>
2022-04-24 09:37:59 -0600

    adaptando el socket

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import ContextSocket from "../../context/context-socketio";
+import ContextChat from "../../context/ChatContext";
+import UserContext from "../../context/UserContext";
+import { useContext, useEffect } from "react";
 //
-
 const chat = () => {
+  
+  const {chats, setChats, } = useContext(ContextChat);
+  const {Socket, conectar, } = useContext(ContextSocket);
+  const user = useContext(UserContext);
+  useEffect(() => {
+    console.log('UseEffect')
+    if (user.logged) {
+      console.log('logged')
+      if (Socket) {
+        console.log('si socket')
+        const handler = (msg) => {
+          setChats([...chats, msg])
+        }
+        Socket.on("pruebaregreso", handler)
+        return () => { 
+          Socket.off("pruebaregreso", handler)
+        }; 
+      } else {
+        conectar()
+      }
+    }
+  }, [chats, Socket, user]);
+
   return (
     <div>
       <div className="row clearfix">
