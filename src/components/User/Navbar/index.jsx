9cfc38dbe5b2192176b9876fea2143167819946b
TiffMonique <tiffanymatamoros72@gmail.com>
2022-05-01 16:59:05 -0600

    Cambios de boton de deslogueo

--- src/components/User/Navbar/index.jsx
+++ src/components/User/Navbar/index.jsx
@@ -12,15 +12,17 @@ import {
   BiHeart,
   BiShoppingBag,
 } from "react-icons/bi";
+import axios from "axios";
+import { FaSignOutAlt } from "react-icons/fa";
 import { style } from "@mui/system";
 
 const Navbar = () => {
   const user = useContext(ContextUser);
-  const buscar = async (categoria, busqueda , departamento) => {
-    const URI = "/Busqueda/?"
-    if(categoria !== "Elige una Cat"){
-      URI = URI + "&categoria="+categoria
-    } 
+  const buscar = async (categoria, busqueda, departamento) => {
+    const URI = "/Busqueda/?";
+    if (categoria !== "Elige una Cat") {
+      URI = URI + "&categoria=" + categoria;
+    }
     if (departamento !== "Elige un Depto") {
       URI = URI + "&departamento=" + departamento;
     }
@@ -35,27 +37,59 @@ const Navbar = () => {
       setBusqueda(response.data);
    */
   };
-  
+
+  const handleClicLogOut = async () => {
+    console.log("logout");
+    await axios
+      .delete("http://localhost:4000/api/tienda/logout", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        user.setlogged(false);
+        user.setadmin(false);
+        user.setidSesion(null);
+        if (Socket) {
+          Socket.disconnect();
+        }
+        swal({
+          title: "LOGOUT EXITOSO",
+          text: response?.data?.message,
+          icon: "success",
+          button: "Aceptar",
+          timer: "1500",
+        });
+        router.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        swal({
+          title: "HA OCURRIDO UN ERROR",
+          text: err.message,
+          icon: "error",
+          button: "Aceptar",
+          timer: "1500",
+        });
+      });
+  };
   useEffect(() => {
     let query = new URLSearchParams(window.location.href);
-    const busqueda = query.get("busqueda")
-    if(busqueda ){
+    const busqueda = query.get("busqueda");
+    if (busqueda) {
       setBusqueda(busqueda);
     }
     setDepartamento("Elige un Depto");
     listacategorias();
-    const depto = query.get("departamento")
-    if(depto){
+    const depto = query.get("departamento");
+    if (depto) {
       setDepartamento(depto);
     }
-    const category = query.get("categoria")
-    if(category){
+    const category = query.get("categoria");
+    if (category) {
       setCategoria(category);
-    } 
+    }
   }, []);
 
   const [categorias, setCategorias] = useState([]);
-  
 
   const listacategorias = async (e) => {
     const response = await fetch(
@@ -66,24 +100,20 @@ const Navbar = () => {
     data.push(catNueva);
     setCategorias(data);
     let query = new URLSearchParams(window.location.href);
-    const category = query.get("categoria")
-    if(category){
+    const category = query.get("categoria");
+    if (category) {
       setCategoria(category);
     } else {
-      setCategoria("Elige una Cat")
+      setCategoria("Elige una Cat");
     }
   };
 
-  
   const [categoria, setCategoria] = useState("");
 
   const [busqueda, setBusqueda] = useState("");
   const [departamento, setDepartamento] = useState("");
 
   const router = useRouter();
-  
-
-  
 
   return (
     <StyledNavbarContainer>
@@ -170,45 +200,43 @@ const Navbar = () => {
         </div>
 
         <div className="content">
-        {
-            (user.logged)?
-          <ul className="ul">
-            <li className="li">
-              <Link href="">
-                <BiShoppingBag size="30" />
-              </Link>
-            </li>
+          {user.logged ? (
+            <ul className="ul">
+              <li className="li">
+                <Link href="">
+                  <BiShoppingBag size="30" />
+                </Link>
+              </li>
 
-            <li className="li">
-              <Link href="/chat/">
-                <BiMessageRounded size="30"/>
-              </Link>
-            </li>
-            <li className="li">
-              <Link href="/">
-                <BiUser size="30" />
-              </Link>
-            </li>
-            <li className="li">
-              <Link href="/user/favoritos">
-                <BiHeart size="30" />
-              </Link>
-            </li>
-          </ul>
-          :
-          <></>
-          }
-          {
-            (user.logged)?
+              <li className="li">
+                <Link href="/chat/">
+                  <BiMessageRounded size="30" />
+                </Link>
+              </li>
+              <li className="li">
+                <a onClick={() => handleClicLogOut()}>
+                  <FaSignOutAlt size="30" />
+                </a>
+              </li>
+              <li className="li">
+                <Link href="/user/favoritos">
+                  <BiHeart size="30" />
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            <></>
+          )}
+          {user.logged ? (
             <div className="loginButtons">
               <Button
                 as="a"
                 width="120px"
                 onClick={() => {
-                  router.push("/#");
+                  router.push("/user/ventas/mostrarVentas");
                 }}
               >
-                Publicar Anuncio
+                Crear Anuncio
               </Button>
               <Button
                 as="a"
@@ -220,25 +248,25 @@ const Navbar = () => {
                 Publicar Producto
               </Button>
             </div>
-            :
+          ) : (
             <ul className="ul">
-            <li className="li">
-              <Link href="/register">
-                <a className="link">Registrar</a>
-              </Link>
-            </li>
-            <li className="li">
-              <Link href="/">
-                <a className="nolin">o</a>
-              </Link>
-            </li>
-            <li className="li">
-              <Link href="/login">
-                <a className="link">Iniciar sesión</a>
-              </Link>
-            </li>
-          </ul>
-          }
+              <li className="li">
+                <Link href="/register">
+                  <a className="link">Registrar</a>
+                </Link>
+              </li>
+              <li className="li">
+                <Link href="/">
+                  <a className="nolin">o</a>
+                </Link>
+              </li>
+              <li className="li">
+                <Link href="/login">
+                  <a className="link">Iniciar sesión</a>
+                </Link>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </StyledNavbarContainer>
--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -513,6 +513,59 @@ const chat = () => {
           </div>
         </div>
       </div>
+      <div class="container py-5">
+        <div class="row justify-content-center mb-3">
+          <div class="col-md-12 col-xl-10">
+            <div class="card shadow-0 border rounded-3">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
+                    <div class="bg-image hover-zoom ripple rounded ripple-surface">
+                      <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/new/img(4).webp"
+                        class="w-100"
+                      />
+                      <a href="#!">
+                        <div class="hover-overlay">
+                          <div
+                            class="mask"
+                            // style="background-color: rgba(253, 253, 253, 0.15);"
+                          ></div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-md-2 col-lg-2 col-xl-2">
+                    <h3>Quant olap shirts</h3>
+
+                    <div class="mt-1 mb-0 text-muted small">
+                      <span>100% cotton</span>
+                    </div>
+
+                    <p class="text-truncate mb-3 mb-md-0">
+                      There are many variations of passages of Lorem Ipsum
+                      available, but the majority have suffered alteration in
+                      some form, by injected humour, or randomised words which
+                      don't look even slightly believable.
+                    </p>
+                  </div>
+                  <div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
+                    <div class="d-flex flex-row align-items-center mb-1">
+                      <h4 class="mb-1 me-1">$14.99</h4>
+                    </div>
+
+                    <div class="d-flex flex-column mt-4">
+                      <button class="btn btn-primary btn-sm" type="button">
+                        Details
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
